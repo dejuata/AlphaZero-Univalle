ee85e30b7ac35cff7272cbb3c0276c89635985fa
Juan Pino <dejuata@hotmail.com>
2018-04-19 19:55:12 -0500

    init project AlphaZero Univalle

---
+++ app/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+from flask import Flask, render_template, request, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
